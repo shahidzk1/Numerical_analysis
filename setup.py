f6fa75645faf5ef5e9a430999d0cbf59be54ecd1
Shahid Khan <47216877+shahidzk1@users.noreply.github.com>
2024-03-05 17:16:42 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Numerical_analysis",
+    version="0.1.0",
+    author="Shahid Khan",
+    author_email='shahidzafarkhan@gmail.com',
+    url='https://github.com/shahidzk1/Numerical_analysis',
+    packages=find_packages(),
+)
